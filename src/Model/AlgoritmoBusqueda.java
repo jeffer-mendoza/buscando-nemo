b87b9eaf16ee652ee488082401c92d7aaa527830
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2016-05-20 11:23:32 -0500

    Se modifican algunos metodos y atributos para hacerlo compatible con la nueva clase Nodo

--- src/Model/AlgoritmoBusqueda.java
+++ src/Model/AlgoritmoBusqueda.java
@@ -10,50 +10,52 @@ public class AlgoritmoBusqueda {
     protected byte matriz[][];//tablero del juego
     protected byte n;//tamano de la matriz
 
-    protected byte metaActual; //objetivo actual que debe ser búscado
-    protected byte numeroMetas = 0;//numero de metas cumplidas
-
     protected long nodoCreados = 0;//los nodos que son creados (size = 2^64)
     protected long nodoExpandidos = 0;//los nodos que son expandidos (size = 2^64)
+    protected long costoTotal = 0;//el costo total de la solución más optima
+    protected byte factorRaminicacion = 0;
+
+    protected byte numeroMetas = 0;//contador que permite conocer cuando se llega a la última meta
 
     //historial de padres
-    protected LinkedList<int[]> historialPadres;//guarda el historial de todos los padres
+    protected LinkedList<Nodo> historialPadres;//guarda el historial de todos los padres
     protected int idsHistorialPadres = 0;
 
     public AlgoritmoBusqueda(byte[][] matriz, byte n) {
         this.matriz = matriz;
         this.n = n;
-        this.numeroMetas = 0;
-        this.metaActual = Personaje.NEMO;
-        this.historialPadres = new LinkedList<int[]>();//se inicializa la cola
+        this.historialPadres = new LinkedList<Nodo>();//se inicializa la cola
         this.idsHistorialPadres = 0;
 
     }
 
     @Override
     public String toString() {
-        return "AlgoritmoBusqueda{" +
-                "Nodos Creados=" + nodoCreados +
-                ", Nodos Expandidos=" + nodoExpandidos +
-                '}';
+        return
+                "\nPasos Solución: "+this.mostrarRuta() +
+                "\nNodos Expandidos: " + this.nodoCreados +
+                "\nNodos Creados: " + this.nodoExpandidos +
+                "\nCosto total Solución: " + this.costoTotal +
+                "\nFactor de Ramificación: " + this.factorRaminicacion;
     }
 
     /**
      * Metodo que permite a partir de la posición de la solucion obtener
      * todos sus ancestros
      */
-    protected void mostrarRuta() {
-        int nodo[] = this.historialPadres.getLast();
-        int id = 0;
+    protected String mostrarRuta() {
+        String ruta = "";
+        Nodo nodo = this.historialPadres.getLast();
+        int index = 0;
         while (true) {
-            System.out.println(nodo[0] + "- (" + nodo[1] + ", " + nodo[2] +")");
-            id = nodo[3];
-            nodo = this.historialPadres.get(id);
-            if(id == 0){
+            ruta += nodo;
+            if(index == 0){
                 break;
             }
+            index = nodo.getPadre();
+            nodo = this.historialPadres.get(index);
         }
-        System.out.println("Root" );
+        return ruta;
     }
 
 }
