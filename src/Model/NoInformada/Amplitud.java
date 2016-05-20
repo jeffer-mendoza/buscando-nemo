14ba718115b2ea29ed6e2a40e14a762c650ead61
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2016-05-20 11:24:15 -0500

    Se adapta el codigo para trabajar con un modelo orientado a objetos que se implementa con la clase nodo

--- src/Model/NoInformada/Amplitud.java
+++ src/Model/NoInformada/Amplitud.java
@@ -2,9 +2,7 @@ package Model.NoInformada;
 
 import Model.Nodo;
 import Model.Personaje;
-
 import java.util.LinkedList;
-
 import Model.AlgoritmoBusqueda;
 
 /**
@@ -16,37 +14,27 @@ public class Amplitud extends AlgoritmoBusqueda {
 
 
     public Amplitud(byte[][] matriz, byte n) {
-        super(matriz,n);
+        super(matriz, n);
         this.cola = new LinkedList<Nodo>();//se inicializa la cola
-        int[] nodo = {0, 0, 0, 0};
+        Nodo nodo = new Nodo(0, (byte) 0, (byte) 0, this.matriz.clone(), (byte) 0, Personaje.NEMO);
+        this.cola.add(nodo);
         this.expandirNodo(nodo);//se expanden los nodos de la raiz
     }
 
 
-
     public void run() {
-        while (this.numeroMetas < 3) {
-            int[] nodo = this.cola.getFirst();//obtiene el nodo de izquierda a derecha
-            if (nodo[0] == this.metaActual) {//verifica si es meta
-                this.numeroMetas++;
+        Nodo nodo;
+        while (true) {
+            nodo = this.cola.getFirst();//obtiene el nodo de izquierda a derecha
+            if (nodo.isMeta()) {//verifica si es meta
                 //todo borrar la meta de la matriz
-                if (this.numeroMetas == 1) {
-                    this.metaActual = Personaje.MARLIN;
-                } else {
-                    if (this.numeroMetas == 2) {
-                        this.metaActual = Personaje.DORI;
-                    }else{
-                        //si se da es porque se cumplieron las tres metas y no permite expandir mas nodos
-                        this.historialPadres.add(nodo);
-                        this.mostrarRuta();
-                        break;
-                    }
+                System.out.print("encontro "+nodo.getMetaActual());
+                if (nodo.isMetaGlobal()) {//verifica si ya se alcanzo la meta global y actualiza las variables de estado
+                    break;
                 }
-                cola = new LinkedList<>();//se reinicia el camino apartir de este nodo
-                this.expandirNodo(nodo);//se expanden los nodos apartir del nodo meta
-                continue;
+                System.out.println(" -- ir por "+nodo.getMetaActual()+ " "+nodo                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 );
+                nodo.mostrarMatriz();
             }
-            cola.removeFirst();//remover este nodo evaluado que no es meta
             this.expandirNodo(nodo);//expandir el nodo
         }
         System.out.println(this.toString());
@@ -61,41 +49,44 @@ public class Amplitud extends AlgoritmoBusqueda {
      * @param nodo
      */
     private void expandirNodo(Nodo nodo) {
+        cola.removeFirst();//remover este nodo evaluado que no es meta
         this.nodoExpandidos++;//se ha expandido un nuevo nodo
         this.historialPadres.add(nodo);//se agrega nodo al historial de padres
         byte i = nodo.getFila();//obtiene la fila del nodo actual
         byte j = nodo.getColumna();//obtiene la columna del noto actual
         if (i - 1 >= 0) {
-            this.crearNodo((byte)(i - 1), j, this.idsHistorialPadres, nodo);
+            this.crearNodo((byte) (i - 1), j, this.idsHistorialPadres, nodo);
         }
         if (i + 1 < this.n) {
-            this.crearNodo((byte)(i + 1), j, this.idsHistorialPadres, nodo);
+            this.crearNodo((byte) (i + 1), j, this.idsHistorialPadres, nodo);
         }
         if (j - 1 >= 0) {
-            this.crearNodo(i, (byte)(j - 1), this.idsHistorialPadres, nodo);
+            this.crearNodo(i, (byte) (j - 1), this.idsHistorialPadres, nodo);
         }
         if (j + 1 < this.n) {
-            this.crearNodo(i, (byte)(j + 1), this.idsHistorialPadres, nodo);
+            this.crearNodo(i, (byte) (j + 1), this.idsHistorialPadres, nodo);
         }
         this.idsHistorialPadres++;//aumenta el identificador de indexamiento
 
     }
 
     /**
-     * Metodo que crea un nuevo nodo
+     * Metodo que crea un nuevo nodo, también se comprueba
+     * que el nodo no se este devolviendo
      *
-     *
-     * @param i      fila de la nueva posición
-     * @param j      columna de la nueva posición
-     * @param padre     identificador del padre
+     * @param i     fila de la nueva posición
+     * @param j     columna de la nueva posición
+     * @param padreId identificador del padre
+     * @param nodoPadre nodo padre del nodo que será creado
      * @return
      */
     private void crearNodo(byte i, byte j, int padreId, Nodo nodoPadre) {
-        Nodo nodo = new Nodo(padreId,i, j,nodoPadre.getMatriz(),nodoPadre.getMetasCumplidas());
-        this.cola.add(nodo);
-        this.nodoCreados++;
+        if(!(i == nodoPadre.getFila() && j == nodoPadre.getColumna())) {
+            Nodo nodo = new Nodo(padreId, i, j, nodoPadre.getMatriz(), nodoPadre.getMetasCumplidas(), nodoPadre.getMetaActual());
+            this.cola.add(nodo);
+            this.nodoCreados++;
+        }
     }
-
 
 
 }
