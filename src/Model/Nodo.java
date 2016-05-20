6d18c90410aee6e0bae72bbaabd14c558d3af5b5
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2016-05-20 11:21:40 -0500

    Se crean los atritubos del metodo nodo que hasta el momento se han identificado

--- src/Model/Nodo.java
+++ src/Model/Nodo.java
@@ -7,21 +7,82 @@ public class Nodo {
     private byte fila;//la posición en la fila
     private byte columna;//la posición de la columna
     private int costo;
-    private boolean[] metasCumplidas;
+    private byte metasCumplidas;
+    private byte metaActual;
     private byte[][] matriz;
+    private long costoAcomulado;//conocer el costo acumulado en el momento
 
-    public Nodo(int padre, byte fila, byte columna, byte[][] matriz, boolean[] metasCumplidas) {
+    public Nodo(int padre, byte fila, byte columna, byte[][] matriz, byte metasCumplidas, byte metaActual) {
         this.padre = padre;
         this.fila = fila;
         this.columna = columna;
         this.matriz = matriz;
         this.metasCumplidas = metasCumplidas;
+        this.metaActual = metaActual;
+        this.personaje = this.matriz[fila][columna];
     }
 
 
     private void expandirNodo() {
 
     }
+
+    /**
+     * Determina si este nodo es una meta
+     *
+     * @return
+     */
+    public boolean isMeta() {
+        if (this.personaje == metaActual) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Actualiza los estados meta, y determina si ya
+     * se alcanzo la meta global, la cual es haber encontrado
+     * a los 3 personajes.
+     *
+     * @return
+     */
+    public boolean isMetaGlobal() {
+        if (this.personaje == Personaje.NEMO) {
+            this.metasCumplidas = 1;
+            this.matriz[this.fila][this.columna]= Personaje.DISPONIBLE;
+            this.metaActual = Personaje.MARLIN;
+
+            return false;
+        }
+        if (this.personaje == Personaje.MARLIN) {
+            this.metasCumplidas = 2;
+            this.metaActual = Personaje.DORI;
+            this.matriz[this.fila][this.columna]= Personaje.DISPONIBLE;
+
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Método que permite mostrar la matriz
+     */
+    public void mostrarMatriz()
+    {
+        int n = this.matriz.length;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
 
 
     /************************
@@ -68,11 +129,11 @@ public class Nodo {
         this.costo = costo;
     }
 
-    public boolean[] getMetasCumplidas() {
+    public byte getMetasCumplidas() {
         return metasCumplidas;
     }
 
-    public void setMetasCumplidas(boolean[] metasCumplidas) {
+    public void setMetasCumplidas(byte metasCumplidas) {
         this.metasCumplidas = metasCumplidas;
     }
 
@@ -82,6 +143,14 @@ public class Nodo {
 
     public void setMatriz(byte[][] matriz) {
         this.matriz = matriz;
+    }
+
+    public byte getMetaActual() {
+        return metaActual;
+    }
+
+    public void setMetaActual(byte metaActual) {
+        this.metaActual = metaActual;
     }
 
     @Override
