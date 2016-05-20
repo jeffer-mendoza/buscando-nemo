d4b3871178defa62e245d6c470346083f02ed894
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2016-05-19 20:31:33 -0500

    Se agrega el metodo getcosto para obtener el costo de cada personaje

--- src/Model/Personaje.java
+++ src/Model/Personaje.java
@@ -2,6 +2,10 @@ package Model;
 
 /**
  * Created by jeffer on 13/05/16.
+ *
+ * Esta clase relaciona el número de identificación de cada personaje del juego
+ * con una constante, lo cual permite un fácil manejo en la manipulación de estos
+ * dentro del código.
  */
 public final class Personaje {
 
@@ -14,6 +18,24 @@ public final class Personaje {
     public static final byte NEMO = 7;
     public static final byte ACUAMAN= 8;
 
+
+    /**
+     * Obtiene el costo de un personaje en particular
+     *
+     * @param personaje
+     * @return
+     */
+    public int getCosto(byte personaje)
+    {
+        if(personaje == TIBURON) {
+            return 10;
+        }
+        if(personaje == ACUAMAN) {
+            return 1000;
+        }
+
+        return 1;
+    }
 
 
 }
